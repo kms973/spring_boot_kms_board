0c78a64ebfccdaffdd40384c09cd8e5636d6cce6
kms973 <135788371+kms973@users.noreply.github.com>
2023-08-31 09:35:04 +0900

    수업

---
+++ src/test/java/edu/global/ex/db/DataSourceTest.java
@@ -0,0 +1,48 @@
+package edu.global.ex.db;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.sql.DataSource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootTest
+class DataSourceTest {
+
+	// CP
+	@Autowired
+	private DataSource datasource;
+
+	@Test
+	void testDataSource() throws Exception {
+		assertNotNull(datasource);
+
+		System.out.println("DS=" + datasource);
+
+		try (Connection conn = datasource.getConnection()) {
+
+			System.out.println("Coooooooooooooooooonnn=" + conn);
+			assertThat(conn).isInstanceOf(Connection.class);
+
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery("select 100 from dual");
+
+			if (rs.next()) {
+				assertEquals(100, rs.getLong(1));
+			}
+
+		}
+
+	}
+}
